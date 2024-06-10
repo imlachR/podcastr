fe0466557d4b6475307a1a414b3f15e220ef57b0
imlachR <rodrimlach@gmail.com>
2024-06-10 13:46:34 -0300

    Add routes and public assets

---
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -0,0 +1,13 @@
+// export default function Page({ params }: { params: { slug: string } }) {
+//   return <div>My Post: {params.slug}</div>
+// }
+
+import React from 'react'
+
+const PodcastDetails = ({ params }: { params: { podcastId: string } }) => {
+  return (
+    <p className='text-white-1'>PodcastDetails for {params.podcastId}</p>
+  )
+}
+
+export default PodcastDetails
